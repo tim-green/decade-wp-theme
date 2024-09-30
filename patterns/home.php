dd3f811f33a398243b36e236e89dc22a2f70005d
Tim <github@timgreen.ws>
2024-09-30 14:35:47 +1000

    added location pattern

---
+++ patterns/home.php
@@ -0,0 +1,32 @@
+<?php declare( strict_types = 1 ); ?>
+<?php
+/**
+ * Title: home
+ * Slug: decade/home
+ * Categories: page
+ * Inserter: yes
+ */
+?>
+<!-- wp:cover {"dimRatio":0,"minHeight":100,"minHeightUnit":"vh","contentPosition":"center center","isDark":false,"style":{"spacing":{"padding":{"right":"5vw","left":"5vw"}}},"textColor":"contrast","layout":{"type":"constrained"}} -->
+<div class="wp-block-cover is-light has-contrast-color has-text-color" style="padding-right:5vw;padding-left:5vw;min-height:100vh"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"align":"full","style":{"dimensions":{"minHeight":"100%"},"spacing":{"padding":{"right":"0vw","left":"0vw"}}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch","verticalAlignment":"space-between"}} -->
+<div class="wp-block-group alignfull" style="min-height:100%;padding-right:0vw;padding-left:0vw"><!-- wp:group {"tagName":"main","style":{"spacing":{"margin":{"top":"14rem","bottom":"14rem"}},"layout":{"selfStretch":"fit","flexSize":null}},"layout":{"type":"constrained","justifyContent":"center"}} -->
+<main class="wp-block-group" style="margin-top:14rem;margin-bottom:14rem"><!-- wp:heading {"textAlign":"center","align":"wide"} -->
+<h2 class="wp-block-heading alignwide has-text-align-center"><?php esc_html_e('Sign up for Decade Digital and guarantee your attendance.', 'decade');?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:group {"align":"wide","layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide"><!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center"><?php esc_html_e('The Future of Online Apprenticeship and Mentorship is a groundbreaking digital conference returning in 2024, poised to redefine how we connect, learn, and collaborate in the rapidly evolving digital landscape.', 'decade');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button"><?php esc_html_e('Sign Up', 'decade');?></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></main>
+<!-- /wp:group -->
+
+<!-- wp:template-part {"slug":"footer","area":"footer"} /--></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover -->
---
+++ patterns/location.php
@@ -0,0 +1,24 @@
+<?php declare( strict_types = 1 ); ?>
+<?php
+/**
+ * Title: location
+ * Slug: decade/location
+ * Categories: text
+ * Inserter: yes
+ */
+?>
+<!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/decade_map.webp" alt="" /></figure>
+<!-- /wp:image -->
+
+<!-- wp:spacer {"height":"1rem"} -->
+<div style="height:1rem" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:paragraph -->
+<p><?php esc_html_e('Located in the heart of Sydney bustling tech scene, South Eveleigh is the perfect venue for the Decade digital conference. Housed in a modern, dynamic building in downtown, this state-of-the-art facility provides a versatile space tailored to foster innovation, collaboration, and networking.', 'decade' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><?php esc_html_e('With its prime location, South Eveleigh offers easy access for both local and international attendees. Situated near major transportation hubs, the venue ensures a seamless and convenient experience for participants, making it simple to arrive and fully enjoy the conference.', 'decade' ); ?></p>
+<!-- /wp:paragraph -->
