0c2914e753845a04c4b8e196192474f6d732c2b5
Tim <github@timgreen.ws>
2024-09-30 14:32:56 +1000

    added faqs pattern

---
+++ patterns/faqs.php
@@ -0,0 +1,44 @@
+<?php declare( strict_types = 1 ); ?>
+<?php
+/**
+ * Title: FAQs
+ * Slug: decade/faqs
+ * Categories: text
+ * Inserter: yes
+ */
+?>
+<!-- wp:details {"summary":"What steps do I take to join the Decade Conference?"} -->
+<details class="wp-block-details"><summary><?php echo esc_html__( 'What steps do I take to join the Decade Conference?', 'decade' ); ?></summary><!-- wp:paragraph -->
+<p><?php echo esc_html__( 'To participate in the Decade Conference, simply register through the official website. After registering, you’ll gain access to the virtual platform, where you can join keynote speeches, engage in panel discussions, attend workshops, and network with other participants.', 'decade' ); ?></p>
+<!-- /wp:paragraph --></details>
+<!-- /wp:details -->
+
+<!-- wp:details {"summary":"What technology do I need to attend the Decade Conference?"} -->
+<details class="wp-block-details"><summary><?php echo esc_html__( 'What technology do I need to attend the Decade Conference?', 'decade' ); ?></summary><!-- wp:paragraph -->
+<p><?php echo esc_html__( 'To attend the Decade Conference, you will need a computer or mobile device with a stable internet connection. The event will be hosted on a web based platform, meaning no software installation is necessary. Just ensure your device has an up to date web browser for the best viewing experience and smooth interaction throughout the conference.', 'decade' ); ?></p>
+<!-- /wp:paragraph --></details>
+<!-- /wp:details -->
+
+<!-- wp:details {"summary":"Will sessions be available for on-demand viewing after the conference?"} -->
+<details class="wp-block-details"><summary><?php echo esc_html__( 'Will sessions be available for on-demand viewing after the conference?', 'decade' ); ?></summary><!-- wp:paragraph -->
+<p><?php echo esc_html__( 'Decade is dedicated to offering a valuable experience for all participants. Most sessions, including keynote speeches and panel discussions, will be recorded and available for on-demand viewing after the conference. This ensures that attendees can catch up on any missed sessions or revisit content at their convenience.', 'decade' ); ?></p>
+<!-- /wp:paragraph --></details>
+<!-- /wp:details -->
+
+<!-- wp:details {"summary":"How can I network and connect with other participants during the Decade Conference?"} -->
+<details class="wp-block-details"><summary><?php echo esc_html__( 'How can I network and connect with other participants during the Decade Conference?', 'decade' ); ?></summary><!-- wp:paragraph -->
+<p><?php echo esc_html__( 'Decade provides numerous networking opportunities within its virtual platform. Attendees can participate in virtual meetups, one-on-one networking sessions, and themed discussion forums to connect with peers, speakers, and industry experts. These features facilitate meaningful interactions and collaboration, mirroring the networking experience of an in-person event.', 'decade' ); ?></p>
+<!-- /wp:paragraph --></details>
+<!-- /wp:details -->
+
+<!-- wp:details {"summary":"Will there be a virtual exhibition or showcase?"} -->
+<details class="wp-block-details"><summary><?php echo esc_html__( 'Will there be a virtual exhibition or showcase?', 'decade' ); ?></summary><!-- wp:paragraph -->
+<p><?php echo esc_html__( 'Decade will include a virtual exhibition area where attendees can explore booths and showcases from innovative startups, established companies, and industry organizations. This interactive space offers live demos, opportunities to engage with exhibitors, and a chance to discover cutting-edge digital technologies and services.', 'decade' ); ?></p>
+<!-- /wp:paragraph --></details>
+<!-- /wp:details -->
+
+<!-- wp:details {"summary":"Can I receive a certificate of attendance or participation for the Decade Conference?"} -->
+<details class="wp-block-details"><summary><?php echo esc_html__( 'Can I receive a certificate of attendance or participation for the Decade Conference?', 'decade' ); ?></summary><!-- wp:paragraph -->
+<p><?php echo esc_html__( 'Yes, Decade offers certificates of attendance or participation for attendees. After the event, you can easily obtain these certificates by completing a request form on the conference website. These certificates serve as proof of your professional development and can be valuable for career advancement or educational purposes.', 'decade' ); ?></p>
+<!-- /wp:paragraph --></details>
+<!-- /wp:details -->
