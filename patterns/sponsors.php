4cf3028546bea7b21b31d65aacc8312b3bfdabf0
Tim <github@timgreen.ws>
2024-09-30 14:39:08 +1000

    added sponsors pattern

---
+++ patterns/sponsors.php
@@ -0,0 +1,19 @@
+<?php declare( strict_types = 1 ); ?>
+<?php
+/**
+ * Title: sponsors
+ * Slug: decade/sponsors
+ * Categories: text
+ * Inserter: yes
+ */
+?>
+
+<!-- wp:paragraph -->
+<p><?php esc_html_e('Decade is proudly supported by a consortium of leading digital brands, showcasing their commitment to innovation and collaboration in the digital landscape..', 'decade' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:group {"style":{"spacing":{"margin":{"top":"10vh","bottom":"10vh"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group" style="margin-top:10vh;margin-bottom:10vh"><!-- wp:image {"align":"center","width":260,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-full is-resized"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/decade_logos.png" alt="" width="260" /></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group -->
