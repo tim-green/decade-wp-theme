c9170f8ac409bf9fd20f789a918b927e94b5b8d8
Tim <github@timgreen.ws>
2024-09-30 14:36:06 +1000

    added speakers pattern

---
+++ patterns/speakers.php
@@ -0,0 +1,102 @@
+<?php declare( strict_types = 1 ); ?>
+<?php
+/**
+ * Title: speakers
+ * Slug: decade/speakers
+ * Categories: text
+ * Inserter: yes
+ */
+?>
+<!-- wp:columns {"align":"wide"} -->
+<div class="wp-block-columns alignwide"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"width":120,"height":120,"sizeSlug":"full","linkDestination":"none","className":"is-style-rounded"} -->
+<figure class="wp-block-image size-full is-resized is-style-rounded"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/speaker_1.webp" alt="" width="120" height="120"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"fontSize":"medium"} -->
+<h2 class="wp-block-heading has-medium-font-size"><?php echo esc_html__( 'Hamish Amore', 'decade' ); ?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php echo esc_html__( 'Hamish Amore, an influential voice in cybersecurity, will discuss the evolving challenges of online security and the necessity for inclusive strategies to protect digital infrastructure. His insights aim to empower individuals and organizations in their efforts to safeguard against cyber threats.', 'decade' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"40px"} -->
+<div style="height:40px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:image {"id":60,"width":120,"height":120,"sizeSlug":"full","linkDestination":"none","className":"is-style-rounded"} -->
+<figure class="wp-block-image size-full is-resized is-style-rounded"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/speaker_6.webp" alt="" class="wp-image-60" width="120" height="120"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"fontSize":"medium"} -->
+<h2 class="wp-block-heading has-medium-font-size"><?php echo esc_html__( 'Natalie Dare', 'decade' ); ?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php echo esc_html__( 'Dare, a leading expert in digital marketing and consumer behavior, will delve into the power of inclusive marketing strategies. He will discuss how businesses can effectively engage diverse audiences in the digital era, emphasizing the importance of understanding and connecting with various consumer segments.', 'decade' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"40px"} -->
+<div style="height:40px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:image {"id":61,"width":120,"height":120,"sizeSlug":"full","linkDestination":"none","className":"is-style-rounded"} -->
+<figure class="wp-block-image size-full is-resized is-style-rounded"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/speaker_3.webp" alt="" class="wp-image-61" width="120" height="120"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"fontSize":"medium"} -->
+<h2 class="wp-block-heading has-medium-font-size"><?php echo esc_html__( 'Alexis Moss', 'decade' ); ?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php echo esc_html__( 'Alexis Moss, a respected expert in human-computer interaction, will explore the principles of inclusive design. She will emphasize the importance of creating digital experiences that are accessible and user-friendly for individuals of all abilities, advocating for a more inclusive approach to technology.', 'decade' ); ?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":57,"width":120,"height":120,"sizeSlug":"full","linkDestination":"none","className":"is-style-rounded"} -->
+<figure class="wp-block-image size-full is-resized is-style-rounded"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/speaker_4.webp" alt="" class="wp-image-57" width="120" height="120"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"fontSize":"medium"} -->
+<h2 class="wp-block-heading has-medium-font-size"><?php echo esc_html__( 'Dr. Skye Rose', 'decade' ); ?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php echo esc_html__( 'Dr. Rose, a trailblazer in the field of virtual reality, will explore how VR can be leveraged for educational purposes. Her discussion will focus on inclusivity in VR experiences and the democratization of knowledge, highlighting the potential of virtual reality to transform learning for diverse audiences.', 'decade' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"40px"} -->
+<div style="height:40px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:image {"id":58,"width":120,"height":120,"sizeSlug":"full","linkDestination":"none","className":"is-style-rounded"} -->
+<figure class="wp-block-image size-full is-resized is-style-rounded"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/speaker_2.webp" alt="" class="wp-image-58" width="120" height="120"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"fontSize":"medium"} -->
+<h2 class="wp-block-heading has-medium-font-size"><?php echo esc_html__( 'James Mccullough', 'decade' ); ?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php echo esc_html__( 'James McCullough, a prominent figure in the world of venture capital, will emphasize the importance of diversity and inclusion in investment decisions. He will advocate for promoting equity and creating opportunities for underrepresented founders, highlighting how diverse perspectives can drive innovation and success in the industry.', 'decade' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"40px"} -->
+<div style="height:40px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:image {"id":59,"width":120,"height":120,"sizeSlug":"full","linkDestination":"none","className":"is-style-rounded"} -->
+<figure class="wp-block-image size-full is-resized is-style-rounded"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/speaker_5.webp" alt="" class="wp-image-59" width="120" height="120"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"fontSize":"medium"} -->
+<h2 class="wp-block-heading has-medium-font-size"><?php echo esc_html__( 'Oscar Lockman', 'decade' ); ?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php echo esc_html__( 'Oscar Lockman, a pioneer in augmented reality, will showcase the transformative potential of AR technologies in crafting inclusive and immersive digital experiences. He will explore how these innovations are revolutionizing industries such as education, healthcare, and entertainment, making them more accessible and engaging for diverse audiences.', 'decade' ); ?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
